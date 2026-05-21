7b30e5381ac2203302c854693afe9223d17856fe
Mahesh Samanapalli <samanapalli.mahesh26@gmail.com>
2026-05-21 13:14:03 +0530

    render bookmarks function added

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,8 +1,27 @@
-async function fetchBookmarks(){
-    const response = await fetch('./data.json');
-    const bookmarks = await response.json();
+async function fetchBookmarks() {
+  try {
+    // Fetch the data from the JSON file
+    const response = await fetch("./data.json");
+    console.log(response);
+    // Check if the response is successful (status code 200-299), handling the http error if not
+    if(!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    const bookmarks = data.bookmarks;
     console.log(bookmarks);
+    // Call the function to render bookmarks
+    renderBookmarks(bookmarks);
+
+  } catch (error) {
+    console.error("Error fetching bookmarks:", error);
+  }
 }
 
 // Call the function to fetch bookmarks when the page loads
 fetchBookmarks();
+
+function renderBookmarks(bookmarks) {
+    console.log("dynamically rendering bookmarks");
+}
+
