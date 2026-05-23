1b54fc80d17a9316ae6d22507f164b52830686d5
Mahesh Samanapalli <samanapalli.mahesh26@gmail.com>
2026-05-23 14:13:25 +0530

    dynamic rendering of the cards with normal approach is done

--- starter-code/script.js
+++ starter-code/script.js
@@ -1,10 +1,11 @@
+const bookmarkContainer = document.getElementById("bookmarkContainer");
 async function fetchBookmarks() {
   try {
     // Fetch the data from the JSON file
     const response = await fetch("./data.json");
     console.log(response);
     // Check if the response is successful (status code 200-299), handling the http error if not
-    if(!response.ok) {
+    if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
@@ -12,7 +13,6 @@ async function fetchBookmarks() {
     console.log(bookmarks);
     // Call the function to render bookmarks
     renderBookmarks(bookmarks);
-
   } catch (error) {
     console.error("Error fetching bookmarks:", error);
   }
@@ -22,9 +22,71 @@ async function fetchBookmarks() {
 fetchBookmarks();
 
 function renderBookmarks(bookmarks) {
-    console.log("dynamically rendering bookmarks");
-    for(let i=0;i<bookmarks.length;i++){
-        console.log(bookmarks[i]);
+  let cardsHtml = "";
+  console.log("dynamically rendering bookmarks");
+  for (let i = 0; i < bookmarks.length; i++) {
+    let bookmark = bookmarks[i];
+    console.log(bookmark);
+    cardsHtml += `
+          <div class="bookmark-card">
+              <div class="card-main-cnt">
+                <div class="card-header">
+                  <div class="card-logo">
+                    <div class="org-logo">
+                      <img
+                        src="${bookmark.favicon}"
+                        alt="${bookmark.title}"
+                      />
+                    </div>
+                    <div class="org-title">
+                      <h3>${bookmark.title}</h3>
+                      <span>${bookmark.url}</span>
+                    </div>
+                  </div>
+                  <div class="edit-dtls">
+                    <img src="./assets/images/icon-menu-bookmark.svg" alt="" />
+                  </div>
+                </div>
+                <div class="card-content">
+                  <p>
+                    ${bookmark.description}
+                  </p>
+                </div>
+                <div class="card-tags">`;
+    for (let j = 0; j < bookmark.tags.length; j++) {
+      cardsHtml += `<span class="tag">${bookmark.tags[j]}</span>`;
     }
+    cardsHtml += `  
+                 </div>
+              </div>
+              <div class="card-footer">
+                <div class="card-details">
+                  <div class="footer-item card-view-count">
+                    <img src="./assets/images/icon-visit-count.svg" alt="" />
+                    <span>${bookmark.visitCount}</span>
+                  </div>
+                  <div class="footer-item card-last-seen">
+                    <img src="./assets/images/icon-last-visited.svg" alt="" />
+                    <span>${formatDate(bookmark.lastVisited)}</span>
+                  </div>
+                  <div class="footer-item card-published">
+                    <img src="./assets/images/icon-created.svg" alt="" />
+                    <span>${formatDate(bookmark.createdAt)}</span>
+                  </div>
+                </div>
+                <div class="card-status">
+                   ${bookmark.pinned ? `<img src="./assets/images/icon-pin.svg" alt="">` : ""}
+                </div>
+              </div>
+            </div>`;
+  }
+  bookmarkContainer.innerHTML = cardsHtml;
 }
+function formatDate(dateString) {
+  const date = new Date(dateString);
 
+  return date.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "short",
+  });
+}
